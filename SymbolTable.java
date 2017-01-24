bc2d908c2cf71321b06af666496bfc9adbba20e3
JBGriffin <JBGriffin@users.noreply.github.com>
2017-01-24 17:35:05 -0600

    Token.java, SymbolTable.java
    More stuff from Larry.

---
+++ SymbolTable.java
@@ -0,0 +1,15 @@
+package havabol;
+
+/**
+ * Currently an empty class due to the IDE screaming at me if there's nothing here. 
+ * It will be used in future projects, but as of now it will be called and does... nothing.
+ * <p>
+ * @author Garrett
+ *
+ */
+public class SymbolTable {
+	public SymbolTable()
+	{
+	
+	}
+}
