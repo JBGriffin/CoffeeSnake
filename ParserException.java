9bd65528e3828d5e02eabe5625bd7476205f20b9
Garrett <garrett.jbgriffin@gmail.com>
2017-01-28 11:39:33 -0600

    Added exception class discussed in class on Thursday.

---
+++ ParserException.java
@@ -0,0 +1,32 @@
+package havabol;
+
+/**
+ * Exception class provided by Larry.
+ */
+public class ParserException extends Exception
+{
+    private int iLineNr;
+    private String diagnostic;
+    private String sourceFileName;
+
+    // constructor
+    public ParserException(int iLineNr, String diagnostic, String sourceFileName)
+    {
+        this.iLineNr = iLineNr;
+        this.diagnostic = diagnostic;
+        this.sourceFileName = sourceFileName;
+    }
+
+    // Exceptions are required to provide tosString()
+    public String toString()
+    {
+        StringBuffer sb = new StringBuffer();
+        sb.append("Line ");
+        sb.append(Integer.toString(iLineNr));
+        sb.append(" ");
+        sb.append(diagnostic);
+        sb.append(", File: ");
+        sb.append(sourceFileName);
+        return sb.toString();
+    }
+}
