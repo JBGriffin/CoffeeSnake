69045dc696663de16df7f279765eae382cfded16
jchooge <jchooge@gvtc.com>
2017-02-01 20:39:53 -0600

    Adding in beginning of STEntries and symbol table methods

---
+++ src/havabol/STFunction.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package havabol;
+
+/**
+ *
+ * @author wimbotexan23
+ */
+public class STFunction extends STEntry {
+    
+    
+    //need to probably changes string... to string[]
+    public STFunction(String szKey, int iPrimClassif, int iType, int iStruct, String... szArgsM) {
+        
+        
+        
+        
+    }
+    
+}
