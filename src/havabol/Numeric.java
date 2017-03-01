37d75ee3a2921c5081e7601746adc8ebd5a06b79
Garrett <garrett.jbgriffin@gmail.com>
2017-03-01 15:13:58 -0600

    Added addition, multiplication, and division methods to numeric class.

--- src/havabol/Numeric.java
+++ src/havabol/Numeric.java
@@ -1,14 +1,14 @@
 package havabol;
 
 /**
- *
+ * Converts given result value strings into a numeric.
  */
 public class Numeric
 {
-    int integerValue;
-    double doubleValue;
-    String strValue;    // display value
-    int type;           // INTEGER, FLOAT
+    private int integerValue;
+    private double doubleValue;
+    private String strValue;    // display value
+    private int type;           // INTEGER, FLOAT
 
     // Error items
     Parser parser;
@@ -18,12 +18,10 @@ public class Numeric
     public Numeric(Parser parser, ResultValue resultValue, String operator, String operand) throws Exception
     {
         this.parser = parser;
-        /*this.resultValue = resultValue;*/
         this.operatorGiven = operator;
         this.position = operand;
         this.type = resultValue.type;
         this.strValue = resultValue.szValue;
-        /*this.resultValue = resultValue;*/
 
         switch(this.type)
         {
@@ -38,6 +36,13 @@ public class Numeric
         }
     }
 
+    private ResultValue setValue(ResultValue resultValue, int type, String szValue)
+    {
+        resultValue.type = type;
+        resultValue.szValue = szValue;
+        return resultValue;
+    }
+
     /**
      * Takes in two operands and returns operand1 subtracted from operand2. If either operand
      * is a double, the result value type will be set to double.
@@ -48,30 +53,149 @@ public class Numeric
     public ResultValue subtract(Numeric operand1, Numeric operand2)
     {
         ResultValue returnValue = new ResultValue("");
+        int intReturn;
+        double dblReturn;
 
         if (operand1.type == Token.INTEGER && operand2.type == Token.INTEGER)
         {
-            int intReturn = operand1.integerValue - operand2.integerValue;
-            returnValue.type = Token.INTEGER;
-            returnValue.szValue = intReturn + "";
+            intReturn = operand1.integerValue - operand2.integerValue;
+            returnValue = setValue(returnValue, Token.INTEGER, intReturn + "");
+
         }
         else if (operand1.type == Token.FLOAT && operand2.type == Token.FLOAT)
         {
-            double dblReturn = operand1.doubleValue - operand2.doubleValue;
-            returnValue.type = Token.FLOAT;
-            returnValue.szValue = dblReturn + "";
+            dblReturn = operand1.doubleValue - operand2.doubleValue;
+            returnValue = setValue(returnValue, Token.FLOAT, dblReturn + "");
         }
         else if(operand1.type == Token.INTEGER && operand2.type == Token.FLOAT)
         {
-            double dblReturn = operand1.integerValue - operand2.doubleValue;
-            returnValue.type = Token.FLOAT;
-            returnValue.szValue = dblReturn + "";
+            dblReturn = operand1.integerValue - operand2.doubleValue;
+            returnValue = setValue(returnValue, Token.FLOAT, dblReturn + "");
         }
         else
         {
-            double dblReturn = operand1.doubleValue - operand2.integerValue;
-            returnValue.type = Token.FLOAT;
-            returnValue.szValue = dblReturn + "";
+            dblReturn = operand1.doubleValue - operand2.integerValue;
+            returnValue = setValue(returnValue, Token.FLOAT, dblReturn + "");
+        }
+
+        return returnValue;
+    }
+
+    /**
+     * Takes in two operands and returns operand1 added to operand2. If either operand
+     * is a double, the result value type will be set to double.
+     * @param operand1 Operand containing integer or double
+     * @param operand2 Operand containing integer or double
+     * @return Returns a ResultValue containing new type and string value.
+     */
+    public ResultValue add(Numeric operand1, Numeric operand2)
+    {
+        ResultValue returnValue = new ResultValue("");
+        int intReturn;
+        double dblReturn;
+
+        if (operand1.type == Token.INTEGER && operand2.type == Token.INTEGER)
+        {
+            intReturn = operand1.integerValue + operand2.integerValue;
+            returnValue = setValue(returnValue, Token.INTEGER, intReturn + "");
+
+        }
+        else if (operand1.type == Token.FLOAT && operand2.type == Token.FLOAT)
+        {
+            dblReturn = operand1.doubleValue + operand2.doubleValue;
+            returnValue = setValue(returnValue, Token.FLOAT, dblReturn + "");
+        }
+        else if(operand1.type == Token.INTEGER && operand2.type == Token.FLOAT)
+        {
+            dblReturn = operand1.integerValue + operand2.doubleValue;
+            returnValue = setValue(returnValue, Token.FLOAT, dblReturn + "");
+        }
+        else
+        {
+            dblReturn = operand1.doubleValue + operand2.integerValue;
+            returnValue = setValue(returnValue, Token.FLOAT, dblReturn + "");
+        }
+        return returnValue;
+    }
+
+    /**
+     * Takes in two operands and returns operand1 multiplied with operand2. If either operand
+     * is a double, the result value type will be set to double.
+     * @param operand1 Operand containing integer or double
+     * @param operand2 Operand containing integer or double
+     * @return Returns a ResultValue containing new type and string value.
+     */
+    public ResultValue multiply(Numeric operand1, Numeric operand2)
+    {
+        ResultValue returnValue = new ResultValue("");
+        int intReturn;
+        double dblReturn;
+
+        if (operand1.type == Token.INTEGER && operand2.type == Token.INTEGER)
+        {
+            intReturn = operand1.integerValue * operand2.integerValue;
+            returnValue = setValue(returnValue, Token.INTEGER, intReturn + "");
+
+        }
+        else if (operand1.type == Token.FLOAT && operand2.type == Token.FLOAT)
+        {
+            dblReturn = operand1.doubleValue * operand2.doubleValue;
+            returnValue = setValue(returnValue, Token.FLOAT, dblReturn + "");
+        }
+        else if(operand1.type == Token.INTEGER && operand2.type == Token.FLOAT)
+        {
+            dblReturn = operand1.integerValue * operand2.doubleValue;
+            returnValue = setValue(returnValue, Token.FLOAT, dblReturn + "");
+        }
+        else
+        {
+            dblReturn = operand1.doubleValue * operand2.integerValue;
+            returnValue = setValue(returnValue, Token.FLOAT, dblReturn + "");
+        }
+
+        return returnValue;
+    }
+
+    /**
+     * Takes in two operands and returns operand1 divided by operand2. If either operand
+     * is a double, the result value type will be set to double.
+     * @param operand1 Operand containing integer or double
+     * @param operand2 Operand containing integer or double
+     * @return Returns a ResultValue containing new type and string value.
+     * @throws Exception Will throw divide by zero exception. Because in java dividing by zero with floats is valid,
+     * will only be thrown for integer division by zero.
+     */
+    public ResultValue divide(Numeric operand1, Numeric operand2) throws Exception
+    {
+        ResultValue returnValue = new ResultValue("");
+        double dblReturn;
+        int intReturn = 0;
+
+        if (operand1.type == Token.INTEGER && operand2.type == Token.INTEGER)
+        {
+            if(operand2.integerValue != 0)
+                intReturn = operand1.integerValue / operand2.integerValue;
+            else
+                parser.errorWithContext("Can not divide by zero! Operands given: " + operand1.integerValue + " and "
+                        + operand2.integerValue);
+
+            returnValue = setValue(returnValue, Token.INTEGER, intReturn + "");
+
+        }
+        else if (operand1.type == Token.FLOAT && operand2.type == Token.FLOAT)
+        {
+            dblReturn = operand1.doubleValue / operand2.doubleValue;
+            returnValue = setValue(returnValue, Token.FLOAT, dblReturn + "");
+        }
+        else if(operand1.type == Token.INTEGER && operand2.type == Token.FLOAT)
+        {
+            dblReturn = operand1.integerValue / operand2.doubleValue;
+            returnValue = setValue(returnValue, Token.FLOAT, dblReturn + "");
+        }
+        else
+        {
+            dblReturn = operand1.doubleValue / operand2.integerValue;
+            returnValue = setValue(returnValue, Token.FLOAT, dblReturn + "");
         }
 
         return returnValue;
